a8f0350abfbd2f5a3abf354c03c78990cd6febda
adelk <adelk@LAPTOP-4RNSPTMS>
2019-02-25 18:47:52 -0700

    Initial Version

---
+++ cse360assign2/cse360assign2/cse360assign2/Calculator.java
@@ -0,0 +1,56 @@
+/*
+ * Adel Karabegovic
+ * Last Edited: 2/18/2019
+ * Description: Calculator program will be using tool versions from
+ * github and applying basic math operations through methods
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	//variables
+	private int total;
+	
+	
+	//default constructor
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//returns total
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	//add to total
+	public void add (int value) 
+	{
+		
+	}
+	
+	//subtract from total
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	//multiply to total
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	//divide from total
+	public void divide (int value) 
+	{
+		
+	}
+	
+	//
+	public String getHistory () 
+	{
+		return "";
+	}
+}
